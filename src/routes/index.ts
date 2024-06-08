aeddf226878a3a30aa31eafac7afffe467f3d7d8
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-06-07 21:07:46 -0300

    Atualização páginas atribuir etapas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import taskRoutes from './task/task.routes'
 import authRoutes from './auth/auth.routes'
 import etapa1Routes from './etapa1/etapa1.routes'
-import etapa2Routes from './etapa2/etapa2.routes'
 import codigoRoutes from './codigo/codigo.routes'
 
 const routes = Router()
@@ -11,6 +10,6 @@ routes.use('/auth', authRoutes)
 routes.use('/task', taskRoutes)
 routes.use('/codigo', codigoRoutes)
 routes.use('/etapa1', etapa1Routes)
-routes.use('/etapa2', etapa2Routes)
+
 
 export default routes
