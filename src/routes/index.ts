3dc2d6ccfae6db427ae50d931984784dc9456272
Pedro Moraes <pm-rogerio@hotmail.com>
2024-06-14 18:56:53 -0300

    ajuste routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import authRoutes from './auth/auth.routes'
 import etapa1Routes from './etapa1/etapa1.routes'
 import etapa2Routes from './etapa2/etapa2.routes'
 import codigoRoutes from './codigo/codigo.routes'
+import homeRoutes from './home/home.routes'
 
 const routes = Router()
 
@@ -12,5 +13,6 @@ routes.use('/task', taskRoutes)
 routes.use('/codigo', codigoRoutes)
 routes.use('/etapa1', etapa1Routes)
 routes.use('/etapa2', etapa2Routes)
+routes.use('/home', homeRoutes)
 
 export default routes
